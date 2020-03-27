43fdbfbb84f65827da0a8f68ef425b816c912670
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-03-27 17:34:02 +0530

    Added:-email validation

--- user_registration.sh
+++ user_registration.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
 validPattern="^[A-Z][a-z]{3,}$"
+
 name=""
 read -p "enter first name: " name
 if [[ $name =~ $validPattern ]]
@@ -15,5 +16,15 @@ then
    echo "valid"
 else
    echo "invalid"
+fi
+
+email=""
+validEmail="^[a-zA-z0-9]{3,}+[@][a-zA-z0-9]{3,}+[.][a-ZA-Z]{3,}$"
+read -p "enter email"  email
+if [[ $email =~ $validEmail ]]
+then
+	echo "valid"
+else
+	echo "invalid"
 fi
 
