8a4f9153b8d911df5d61c315944b6799e00de514
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-03-27 14:52:57 +0530

    Added:-validation last name

--- user_registration.sh
+++ user_registration.sh
@@ -1,10 +1,19 @@
 #!/bin/bash -x
 validPattern="^[A-Z][a-z]{3,}$"
 name=""
-read -p "enter name " name
+read -p "enter first name: " name
 if [[ $name =~ $validPattern ]]
 then
 	echo "valid"
 else
 	echo "invalid"
 fi
+lastName=""
+read -p "enter last name: " lastName
+if [[ $lastName =~ $validPattern ]]
+then
+   echo "valid"
+else
+   echo "invalid"
+fi
+
