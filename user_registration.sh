ca6c9b7ae3f7a0c91ad987473b271a6fb50babf1
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-03-29 11:42:37 +0530

    refactor:-only use function amd call many times

--- user_registration.sh
+++ user_registration.sh
@@ -1,49 +1,39 @@
 #!/bin/bash -x
 validPattern="^[A-Z][a-z]{3,}$"
-
-name=""
-read -p "enter first name: " name
-if [[ $name =~ $validPattern ]]
-then
-	echo "valid"
-else
-	echo "invalid"
-fi
-lastName=""
-read -p "enter last name: " lastName
-if [[ $lastName =~ $validPattern ]]
-then
-   echo "valid"
-else
-   echo "invalid"
-fi
-
-email=""
 EmailPattern1="^[a-zA-z0-9]{3,}+[@]{1}"
 EmailPattern2="^[a-zA-z0-9]{3,}+[@]{1}[a-zA-z0-9]{3,}"
 EmailPattern3="^[a-zA-z0-9]{3,}+[@]{1}[a-zA-z0-9]{3,}+[.][a-ZA-Z]{2,4}$"
 validEmail="^[a-zA-z0-9]{3,}+[@][a-zA-z0-9]{3,}+[.][a-ZA-Z]{2,4}$"
-read -p "enter email"  email
-if [[ $email =~ $validEmail ]]
-then
-	echo "valid"
-else
-	echo "invalid"
-fi
+validNumber="^[0-9]{2} [0-9]{10,10}$"
 
-mobileNumber=""
-validNumber="^[0-9]{2} [0-9]{10}$"
-read -p "enter mobile number: " mobileNumber
-if [[ $mobileNumber =~ $validNumber ]]
+function validation(){
+if [[ $1 =~ $2 ]]
 then
    echo "valid"
 else
    echo "invalid"
 fi
 
+}
+name=""
+read -p "enter first name: " name
+validation $name $validpattern
+
+lastName=""
+read -p "enter last name: " lastName
+validation $lastName $validpattern
+
+email=""
+read -p "enter email"  email
+validation $email $validEmail
+
+mobileNumber=""
+read -p "enter mobile number: " mobileNumber
+validation $mobileNumber $validNumber
+
 password=""
 read -p "enter password: " password
-if [[ ${#password} -gt 7 && "$password" == *[A-Z]* && "$password" == *[0-9]* && *[\!\@\#\%\*\&\$\=\-]*{1] ]]
+if [[ ${#password} -gt 7 && "$password" == *[A-Z]* && "$password" == *[0-9]* && *[\!\@\#\%\*\&\$\=\-]*{1,1} ]]
 then
 echo "valid"
 else
